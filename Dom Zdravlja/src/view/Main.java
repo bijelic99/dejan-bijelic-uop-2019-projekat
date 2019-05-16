c71ad79356dd945939fb8beb361a92514c71258c
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2019-05-16 17:17:37 +0200

    system look and feel

--- Dom Zdravlja/src/view/Main.java
+++ Dom Zdravlja/src/view/Main.java
@@ -1,11 +1,20 @@
 package view;
 
+import javax.swing.UnsupportedLookAndFeelException;
+
 import controller.DataStore;
 import controller.Router;
 
 public class Main {
 
 	public static void main(String[] args) {
+		try {
+			javax.swing.UIManager.setLookAndFeel( javax.swing.UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		DataStore.initialLoad();
 		//System.out.println(DataStore.pacijenti);
 		//System.out.println(DataStore.medicinskeSestre);
